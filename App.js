9b80c1663dce12411123881fa0954ae3c89e362d
mlsniperpro <peterokoth.km@gmail.com>
2022-05-12 12:17:13 -0700

    Both home and recognize music pages

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './app/screens/Home'
+import RecognizeMusic from './app/screens/RecognizeMusic'
 export default function App() {
   return (
-    <Home/>
+    <RecognizeMusic/>
   );
 }
 
