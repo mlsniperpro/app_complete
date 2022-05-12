9105be1dd4897c914a17dec351dc77c32de11490
mlsniperpro <peterokoth.km@gmail.com>
2022-05-12 12:39:40 -0700

    The ui working

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './app/screens/Home'
-import RecognizeMusic from './app/screens/RecognizeMusic'
+import Home from './app/screens/Home';
+import RecognizeMusic from './app/screens/RecognizeMusic';
+import MusicPlayer from './app/screens/MusicPlayer';
 export default function App() {
   return (
-    <RecognizeMusic/>
+    <MusicPlayer/>
   );
 }
 
