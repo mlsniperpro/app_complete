7c44c87d6361afdc19f69f901ffe0a0f6fa2326e
mlsniperpro <peterokoth.km@gmail.com>
2022-05-13 07:22:53 +0300

    Styled recognition button

--- app/screens/RecognizeMusic.js
+++ app/screens/RecognizeMusic.js
@@ -1,54 +1,61 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
+import {StyleSheet, View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import Button from "../components/Button";
+import {Audio} from 'expo-av';
 
-export default function App() {
-  const [recording, setRecording] = React.useState();
-
-  async function startRecording() {
-    try {
-      console.log('Requesting permissions..');
-      await Audio.requestPermissionsAsync();
+async function startRecording(){
+  try {
+    const permission = await Audio.requestPermissionsAsync();
+    if (permission.status=="granted"){
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      }); 
-      console.log('Starting recording..');
+        playsInSilentModeIOS: true
+      });
       const { recording } = await Audio.Recording.createAsync(
-         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-      );
-      setRecording(recording);
-      console.log('Recording started');
-      setTimeout(()=>{return("Hello")}, 1000)
-    } catch (err) {
-      console.error('Failed to start recording', err);
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+        setRecording(recording);
+    } else {
+      setMessage("Please grant the microphone permission to proceed")
     }
+    
+  }catch(err){
+    console.error("Failed to initiate recognition", err)
   }
+}
+async function stopRecording(){
+  setRecording(undefined);
+  await recording.stopUnloadAsync();
 
-  async function stopRecording() {
-    console.log('Stopping recording..');
-    setRecording(undefined);
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI();
-    console.log('Recording stopped and stored at', uri);
- 
+  let updateRcordings = [...recordings];
+  const {sound, status} = await recording.createNewLoadedSoundAsync();
   }
-
+export default function RecognizeMusic() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState();
+  const [message, setMessage] = React.useState("");
   return (
-    <View style={styles.container}>
-      <Button
-        title={recording ? 'Stop Recording' : 'Start Recording'}
-        onPress={startRecording}
-      />
+    <View>
+       <View style={styles.buttonsContainer}>
+       <Button
+       title={recording? "Print Details":"Initiate Recognition"}
+       onPress={recording ? stopRecording : startRecording}
+
+       />
+       <StatusBar style='auto'/>
+       </View>
+       <Text>{message}</Text>
+       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
-  },
-});
+  buttonsContainer:{
+    padding: 20,
+    width: "100%",
+
+},
+
+})
