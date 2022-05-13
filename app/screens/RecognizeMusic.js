5dc068d6fb9bdf8e973d8df4b99aa6fba0328bdb
mlsniperpro <peterokoth.km@gmail.com>
2022-05-13 04:13:18 +0300

    All pages working

--- app/screens/RecognizeMusic.js
+++ app/screens/RecognizeMusic.js
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
-export default function RecognizeMusic() {
+export default function App() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+      setTimeout(()=>{return("Hello")}, 1000)
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+ 
+  }
+
   return (
-    <View>
-      <Text>RecognizeMusic</Text>
+    <View style={styles.container}>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={startRecording}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+});
